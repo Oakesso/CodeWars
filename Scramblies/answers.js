8ad133807d78f696334859c36586d997174685a1
Oakesso <olivierakesso@gmail.com>
2021-11-04 22:53:50 +0100

    answer to javascript kyu 5 - Scramblies

--- Scramblies/answers.js
+++ Scramblies/answers.js
@@ -1,21 +1,21 @@
-// Complete the
-// function scramble(str1, str2) that returns true
-// if a portion of str1 characters can be rearranged
-// to match str2, otherwise returns false.
-
-// Notes:
-// Only lower case letters will be used(a - z).
-// No punctuation or digits will be included.
-// Performance needs to be considered
-// Input strings s1 and s2 are null terminated.
-// Examples
-// scramble('rkqodlw', 'world') == > True
-// scramble('cedewaraaossoqqyt', 'codewars') == > True
-// scramble('katas', 'steak') == > False
+let str1 = "cedewaraaossoqqyt";
+let str2 = "codewars";
 
 function scramble(str1, str2) {
-    for (let i = 0; i < str1.length; i++) {
-        console.log(str2.length);
+    let trueList = [];
+    for (let i = 0; i < str2.length; i++) {
+        if (str1.includes(str2[i])) {
+            trueList.push(true);
+        } else {
+            trueList.push(false);
+        }
+        for (let j = 0; j < str2.length; j++) {
+            if (trueList[j] != true) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 }
 
