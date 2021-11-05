b1936bf9bb8d7311914cd9c789d2efd8c48cd182
Oakesso <olivierakesso@gmail.com>
2021-11-05 01:13:27 +0100

    answer to javascript kyu 5 - Scramblies

--- Scramblies/answers.js
+++ Scramblies/answers.js
@@ -1,22 +1,105 @@
-let str1 = "cedewaraaossoqqyt";
-let str2 = "codewars";
+let str1 = "aabbcamaomsccdd";
+let str2 = "commas";
 
 function scramble(str1, str2) {
-    let trueList = [];
-    for (let i = 0; i < str2.length; i++) {
-        if (str1.includes(str2[i])) {
-            trueList.push(true);
-        } else {
-            trueList.push(false);
-        }
-        for (let j = 0; j < str2.length; j++) {
-            if (trueList[j] != true) {
-                return false;
+    let firstList = [];
+    let secondList = [];
+    if (str1.length !== str2.length) {
+        console.log("different length:", str1, str2);
+        for (let i = 0; i < str2.length; i++) {
+            if (str1.includes(str2[i])) {
+                firstList.push(true);
             } else {
-                return true;
+                firstList.push(false);
             }
+        }
+
+        for (let j = 0; j < str1.length; j++) {
+            if (str2.includes(str1[j])) {
+                secondList.push(true);
+            } else {
+                secondList.push(false);
+            }
+        }
+
+        console.log(firstList, secondList);
+
+        if (firstList.includes(false) || secondList.includes(false)) {
+            console.log("case 1 : ");
+            return false;
+        } else if (firstList.includes(true) || secondList.includes(false)) {
+            console.log("case 2 : ");
+            return true;
+        } else if (firstList.includes(false) || secondList.includes(true)) {
+            console.log("case 3 : ");
+            return false;
+        } else if (firstList.includes(true) || secondList.includes(true)) {
+            console.log("case 4 : ");
+            return true;
+        }
+    } else if (str1.length === str2.length) {
+        console.log("equal length :", str1, str2);
+        for (let i = 0; i < str2.length; i++) {
+            if (str1.includes(str2[i])) {
+                firstList.push(true);
+            } else {
+                firstList.push(false);
+            }
+        }
+
+        for (let j = 0; j < str1.length; j++) {
+            if (str2.includes(str1[j])) {
+                secondList.push(true);
+            } else {
+                secondList.push(false);
+            }
+        }
+        console.log(firstList, secondList);
+        if (firstList.includes(false) & secondList.includes(false)) {
+            console.log("case 1 : ");
+            return false;
+        } else if (firstList.includes(true) & secondList.includes(false)) {
+            console.log("case 2 : ");
+            return false;
+        } else if (firstList.includes(false) & secondList.includes(true)) {
+            console.log("case 3 : ");
+            return false;
+        } else if (firstList.includes(true) & secondList.includes(true)) {
+            console.log("case 4 : ");
+            return true;
         }
     }
 }
 
-scramble(str1, str2);
+console.log(scramble(str1, str2));
+
+// function scramble(str1, str2) {
+//     console.log(str1, str2);
+//     let firstList = [];
+//     let secondList = [];
+//     for (let i = 0; i < str2.length; i++) {
+//         if (str1.includes(str2[i])) {
+//             firstList.push(true);
+//         } else {
+//             firstList.push(false);
+//         }
+//     }
+
+//     for (let j = 0; j < str1.length; j++) {
+//         if (str2.includes(str1[j])) {
+//             secondList.push(true);
+//         } else {
+//             secondList.push(false);
+//         }
+//     }
+//     console.log(firstList, secondList);
+//     if (firstList.includes(false) || secondList.includes(false)) {
+//         return false;
+//     } else if (firstList.includes(true) || secondList.includes(false)) {
+//         return false;
+//     } else if (firstList.includes(false) || secondList.includes(true)) {
+//         return false;
+//     } else if (firstList.includes(true) || secondList.includes(true)) {
+//         return true;
+//     }
+// }
